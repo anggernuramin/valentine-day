01b9f3d801a6024e89481088c42b9e7bfe7caa77
angger <angger@newtronic-solution.com>
2025-02-14 07:26:44 +0700

    edit window

--- src/util/Confetti.tsx
+++ src/util/Confetti.tsx
@@ -1,19 +1,29 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Confetti from "react-confetti";
 
 const Surprise = () => {
   const confettiRef = useRef(null);
+  const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
 
   useEffect(() => {
-    const confetti = confettiRef.current as HTMLCanvasElement | null;
-    const context = confetti?.getContext("2d");
+    if (typeof window !== "undefined") {
+      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+
+      // Tambahkan event listener untuk resize window
+      const handleResize = () => {
+        setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+      };
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
   }, []);
+
   return (
     <Confetti
-      width={window?.innerWidth}
+      width={windowSize.width}
+      height={windowSize.height}
       ref={confettiRef}
       numberOfPieces={150}
     />
